3dcf9e5a693cfe2ce4d2ddeb5e0cd88f5d1f0a4b
evuke99 <65368200+evuke99@users.noreply.github.com>
2021-04-09 09:31:08 -0400

    Final Update for the NetworkManager.cs

--- FinalDownloadables/NetworkManager.cs
+++ FinalDownloadables/NetworkManager.cs
@@ -1,17 +1,30 @@
-﻿using UnityEngine;
-using System.Collections;
+using UnityEngine;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Net;
 
 public class NetworkManager : MonoBehaviour
 {
     InputController _inputController;
+
+    [SerializeField]
+    public static string IP = "192.168.1.239";
+
+    //if the variable IP does not work there has been issues with that in the past, just replace it directly with the string
+    private System.Net.IPAddress Address = System.Net.IPAddress.Parse(IP);
+
+    [SerializeField]
+    public int port = 80;
 
 
     void Start()
     {
         // This will do the network stuff
         _inputController = new InputController();
-        _inputController.Begin("192.168.1.231", 80);
+        _inputController.Begin(Address, port);
     }
 
     void Update()
